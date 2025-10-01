236447b14a372673d9d8ca54abc7381153dff26e
mvgarc <mariav2601@gmail.com>
2025-10-01 19:10:52 -0400

    Add initial variable definitions and comments in variables-practica.py

---
+++ variables-practica.py
@@ -0,0 +1,22 @@
+#1nombre = "Carla"
+nombre_1 = "Carla"
+#nombre 1 = "juan"
+#Variables
+"""
+wehfdeuihfe
+febfbefbe
+
+"""
+
+nombreCompleto = "Juan Perez"
+NombreCompleto = "Luis Garcia"
+Nombre = "Andres"
+nombre = "Maria"
+numero = 12
+decimal = 3.58
+a = "Paola"
+print (nombre, "" , numero, decimal)
+print ("Hola, me llamo", nombre)
+print ("Tengo",  numero , "años")
+print (numero + decimal)
+print ("hola" + " " +  nombre)
