1f3f9d23ee1fb12d7aebc63d5c3b6daa4351b8d5
mvgarc <mariav2601@gmail.com>
2025-10-01 21:38:13 -0400

    Creacion de archivos para explicar funciones en python

---
+++ funciones.py
@@ -0,0 +1,15 @@
+"""def saludar(nombre):
+    print(f"Hola, {nombre}!")
+
+def despedir(nombre):
+    print(f"Adiós, {nombre}!")
+
+saludar("Ana")
+despedir("Ana")"""
+
+def es_par_o_impar(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+    
---
+++ funciones_2.py
@@ -0,0 +1,4 @@
+import funciones
+
+resultado = funciones.es_par_o_impar(3)
+print(f"El número es: {resultado}")
